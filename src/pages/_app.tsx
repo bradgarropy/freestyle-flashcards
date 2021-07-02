14f0317e81bd4c699b84534b71d9351f17a06059
Brad Garropy <bradgarropy@gmail.com>
2021-07-01 22:26:39 -0500

    switch to next-seo package. (#4)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,34 @@
 import "../styles/styles.css"
 
+import SEO from "@bradgarropy/next-seo"
 import type {AppProps} from "next/app"
 import {ReactElement} from "react"
 
+import pkg from "../../package.json"
+
 const App = ({Component, pageProps}: AppProps): ReactElement => {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <SEO
+                title={pkg.name}
+                description={pkg.description}
+                keywords={pkg.keywords}
+                icon="/microphone.png"
+                facebook={{
+                    image: "https://freestyle-flashcards.vercel.app/facebook.png",
+                    url: "https://freestyle-flashcards.vercel.app",
+                    type: "website",
+                }}
+                twitter={{
+                    image: "https://freestyle-flashcards.vercel.app/twitter.png",
+                    site: "@bradgarropy",
+                    card: "summary",
+                }}
+            />
+
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default App
